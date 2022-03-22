b05074d134c0298dfc0db0b5218973b3880ec35f
Yoo-Jin Lee <yj@mokten.com>
2022-03-23 09:27:49 +1100

    kick

--- step.sh
+++ step.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. ./utils.sh
 
 #=======================================
 # Upload an iOS or Android file to TestProject
@@ -8,77 +9,6 @@
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -e
-
-#=======================================
-# Functions
-#=======================================
-
-RESTORE='\033[0m'
-RED='\033[00;31m'
-YELLOW='\033[00;33m'
-BLUE='\033[00;34m'
-GREEN='\033[00;32m'
-
-function color_echo {
-	color=$1
-	msg=$2
-	echo -e "${color}${msg}${RESTORE}"
-}
-
-function echo_fail {
-	msg=$1
-	echo
-	color_echo "${RED}" "${msg}"
-	exit 1
-}
-
-function echo_warn {
-	msg=$1
-	color_echo "${YELLOW}" "${msg}"
-}
-
-function echo_info {
-	msg=$1
-	echo
-	color_echo "${BLUE}" "${msg}"
-}
-
-function echo_details {
-	msg=$1
-	echo "  ${msg}"
-}
-
-function echo_done {
-	msg=$1
-	color_echo "${GREEN}" "  ${msg}"
-}
-
-function validate_required_input {
-	key=$1
-	value=$2
-	if [ -z "${value}" ] ; then
-		echo_fail "[!] Missing required input: ${key}"
-	fi
-}
-
-function validate_required_input_with_options {
-	key=$1
-	value=$2
-	options=$3
-
-	validate_required_input "${key}" "${value}"
-
-	found="0"
-	for option in "${options[@]}" ; do
-		if [ "${option}" == "${value}" ] ; then
-			found="1"
-		fi
-	done
-
-	if [ "${found}" == "0" ] ; then
-		echo_fail "Invalid input: (${key}) value: (${value}), valid options: ($( IFS=$", "; echo "${options[*]}" ))"
-	fi
-}
 
 #=======================================
 # Main
@@ -130,3 +60,6 @@ else
 
     echo_details "* TESTPROJECT_JOB_RESULT:     $TESTPROJECT_JOB_RESULT"
 fi
+
+curl -u "$browserstack_username:$browserstack_access_key" -X POST https://api-cloud.browserstack.com/app-live/upload -F "file=@$apk_ipa_filepath"
+
