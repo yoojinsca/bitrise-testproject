976f01a66408e1b838bb2260b723706a9680fe8f
Yoo-Jin Lee <yj@mokten.com>
2022-03-22 12:57:49 +1100

    kick

--- step.sh
+++ step.sh
@@ -1,38 +1,95 @@
 #!/bin/bash
 
-set -eox
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-# envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+set -e
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+#=======================================
+# Functions
+#=======================================
 
-# https://api.testproject.io/docs/v2/
+RESTORE='\033[0m'
+RED='\033[00;31m'
+YELLOW='\033[00;33m'
+BLUE='\033[00;34m'
+GREEN='\033[00;32m'
 
-echo "apk_ipa_filepath:         $apk_ipa_filepath"
-echo "testproject_access_key:   $testproject_access_key"
-echo "testproject_project_id:   $testproject_project_id"
-echo "testproject_app_id:       $testproject_app_id"
-echo "testproject_filename:     $testproject_filename"
+function color_echo {
+	color=$1
+	msg=$2
+	echo -e "${color}${msg}${RESTORE}"
+}
 
-envman run bash -c 'echo "testproject_access_key: ${testproject_access_key}"'
-envman run bash -c 'echo "testproject_project_id: ${testproject_project_id}"'
-envman run bash -c 'echo "testproject_app_id: ${testproject_app_id}"'
-envman run bash -c 'echo "testproject_filename: ${testproject_filename}"'
-envman run bash -c 'echo "BITRISE_IPA_PATH: $BITRISE_IPA_PATH"'
+function echo_fail {
+	msg=$1
+	echo
+	color_echo "${RED}" "${msg}"
+	exit 1
+}
+
+function echo_warn {
+	msg=$1
+	color_echo "${YELLOW}" "${msg}"
+}
+
+function echo_info {
+	msg=$1
+	echo
+	color_echo "${BLUE}" "${msg}"
+}
+
+function echo_details {
+	msg=$1
+	echo "  ${msg}"
+}
+
+function echo_done {
+	msg=$1
+	color_echo "${GREEN}" "  ${msg}"
+}
+
+function validate_required_input {
+	key=$1
+	value=$2
+	if [ -z "${value}" ] ; then
+		echo_fail "[!] Missing required input: ${key}"
+	fi
+}
+
+function validate_required_input_with_options {
+	key=$1
+	value=$2
+	options=$3
+
+	validate_required_input "${key}" "${value}"
+
+	found="0"
+	for option in "${options[@]}" ; do
+		if [ "${option}" == "${value}" ] ; then
+			found="1"
+		fi
+	done
+
+	if [ "${found}" == "0" ] ; then
+		echo_fail "Invalid input: (${key}) value: (${value}), valid options: ($( IFS=$", "; echo "${options[*]}" ))"
+	fi
+}
+
+#=======================================
+# Main
+#=======================================
+
+echo_details "* apk_ipa_filepath:         $apk_ipa_filepath"
+echo_details "* testproject_access_key:   $testproject_access_key"
+echo_details "* testproject_project_id:   $testproject_project_id"
+echo_details "* testproject_app_id:       $testproject_app_id"
+echo_details "* testproject_filename:     $testproject_filename"
+
+validate_required_input "apk_ipa_filepath" $apk_ipa_filepath
+validate_required_input "testproject_access_key" $testproject_access_key
+validate_required_input "testproject_project_id" $testproject_project_id
+validate_required_input "testproject_app_id" $testproject_app_id
+validate_required_input "testproject_filename" $testproject_filename
 
 # if [ -z "$apk_ipa_filepath" ]; then
 #   echo "Please provide the path for the IPA or APK that you wish to upload."
